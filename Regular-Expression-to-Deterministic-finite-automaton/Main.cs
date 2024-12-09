37cfe9c321bb6eca7c39ea7f73153860764a441e
dan <dan.ignat@student.unitbv.ro>
2024-12-09 20:41:23 +0200

    miau

--- Regular-Expression-to-Deterministic-finite-automaton/Main.cs
+++ Regular-Expression-to-Deterministic-finite-automaton/Main.cs
@@ -1,6 +1,7 @@
 ﻿using dfa;
 using System;
 using System.ComponentModel;
+using System.IO;
 
 namespace dfa
 {
@@ -9,38 +10,26 @@ namespace dfa
 
         public static void Main(string[] args)
         {
-
-
-            // Console.WriteLine(parser.PolishForm);
-
-            //NFA lhs = new NFA('a', 0);
-
-            //NFA rhs = new NFA('b', 2);
-
-            //NFA alternated = NFA.Alternate(lhs, rhs, 4);
-
-
-
-
-            //NFA miau = new NFA('c', 6);
-            //Console.WriteLine(factory.nfa);
-
-            string expr = "(a|b)c*d*";
-
-            Regex regex = new Regex(expr);
-
-            while (true) {
-                string line = Console.ReadLine();
-                Console.WriteLine(regex.regexMatch(line));
+            string path = "input.txt";
+            string content;
+            if (File.Exists(path)) {
+                content = File.ReadAllText(path);
+                Console.WriteLine(content);
+            }
+            else {
+                throw new Exception("File not found");
             }
 
-            //Parser parser = new Parser(expr);
+            Regex regex = new Regex(content);
+            Console.WriteLine($"Expresia regulata: {content}");
 
-            //Console.WriteLine(parser.PolishForm);
-
-            //DFAFactory factory_dfa = new DFAFactory(expr);
-
-            //Console.WriteLine(factory_dfa.dfa);
+            while (true) {
+                Console.WriteLine("Introdu test");
+                string line = Console.ReadLine();
+                Console.WriteLine($"regex match: {regex.regexMatch(line)}\n");
+            }
         }
+
+        
     }
 }
